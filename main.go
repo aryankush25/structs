0c82aea3a8f9fa3416c32e7e37aa132d7cc4dac0
Aryan Agarwal <aryankush025@gmail.com>
2024-03-19 17:53:56 +0530

    Struct in struct

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import "fmt"
 
+type contact struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contact
 }
 
 func main() {
-	p1 := person{firstName: "Alex", lastName: "Anderson"}
+	p1 := person{firstName: "Alex", lastName: "Anderson", contact: contact{email: "alex@gmail.com", zipCode: 12345}}
+
+	fmt.Println(p1)
 
 	p1.print()
 }
 
 func (p person) print() {
 	fmt.Println(p.firstName, p.lastName)
+	p.contact.print()
+}
+
+func (c contact) print() {
+	fmt.Println(c.email, c.zipCode)
 }
